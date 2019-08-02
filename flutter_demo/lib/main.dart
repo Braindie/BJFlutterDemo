bbf81b8aa526d9b51efd2d9eaf6307e2d11fd955
zhangwenjun <zhangwenjun@micego.com>
2019-08-02 19:52:00 +0800

    Flutter语法

--- flutter_demo/lib/main.dart
+++ flutter_demo/lib/main.dart
@@ -1,20 +1,85 @@
 import 'package:flutter/material.dart';
+// import 'package:english_words/english_words.dart';
 
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    // final wordPair = new WordPair.random();
     return new MaterialApp(
       title: 'Welcome to Flutter',
-      home: new Scaffold(
-        appBar: new AppBar(
-          title: new Text('Welcome to Flutter'),
-        ),
-        body: new Center(
-          child: new Text('World'),
+      home: new MyScaffold(),
+      // home: new Scaffold(
+      //   appBar: new AppBar(
+      //     title: new Text('Welcome to Flutter'),
+      //   ),
+      //   body: new Center(
+      //     child: new Text('World'),
+      //   ),
+      // ),
+    );
+  }
+}
+
+class MyScaffold extends StatelessWidget {
+
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle = TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Index 0: Home',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 1: Business',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 2: School',
+      style: optionStyle,
+    ),
+  ];
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: new Text(
+          '首页'
         ),
       ),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            title: Text('Home'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.business),
+            title: Text('Business'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.school),
+            title: Text('School')
+          )
+        ],
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+      ),
     );
+  }
+
+  void _onItemTapped(int index) {
+    print(index);
+    print(_selectedIndex);
+    // setState(() {
+    //   _selectedIndex = index;
+    // });
   }
 }
